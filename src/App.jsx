cf2332ce05abc97e5830bec7324632c399b0a5da
Blessingkeyz <umukoroblessing@gmail.com>
2022-12-31 03:04:08 -0800

    all

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calculator from './components/Calculator'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Calculator />
+    </>
+  )
+}
+
+export default App
